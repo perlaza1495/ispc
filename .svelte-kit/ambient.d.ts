0e4d71711a2c4ccb32408eee261b285e8f7d9b35
perlaza1495 <alejabcperlaza@gmail.com>
2022-08-13 16:40:54 -0500

    Initial commit

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,65 @@
+// this file is generated — do not edit it
+/// <reference types="@sveltejs/kit" />
+
+declare module '$env/static/public' {
+
+}
+
+declare module '$env/static/private' {
+	export const TERM_PROGRAM: string;
+	export const NODE: string;
+	export const INIT_CWD: string;
+	export const TERM: string;
+	export const SHELL: string;
+	export const npm_config_metrics_registry: string;
+	export const TMPDIR: string;
+	export const npm_config_global_prefix: string;
+	export const TERM_PROGRAM_VERSION: string;
+	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
+	export const npm_package_optional: string;
+	export const COLOR: string;
+	export const npm_config_noproxy: string;
+	export const npm_config_local_prefix: string;
+	export const USER: string;
+	export const COMMAND_MODE: string;
+	export const npm_config_globalconfig: string;
+	export const npm_package_peer: string;
+	export const SSH_AUTH_SOCK: string;
+	export const __CF_USER_TEXT_ENCODING: string;
+	export const npm_execpath: string;
+	export const npm_package_integrity: string;
+	export const PATH: string;
+	export const npm_package_json: string;
+	export const _: string;
+	export const npm_config_userconfig: string;
+	export const npm_config_init_module: string;
+	export const __CFBundleIdentifier: string;
+	export const npm_command: string;
+	export const PWD: string;
+	export const npm_lifecycle_event: string;
+	export const EDITOR: string;
+	export const npm_package_name: string;
+	export const LANG: string;
+	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+	export const XPC_FLAGS: string;
+	export const npm_package_engines_node: string;
+	export const npm_config_node_gyp: string;
+	export const npm_package_dev: string;
+	export const npm_package_version: string;
+	export const XPC_SERVICE_NAME: string;
+	export const npm_package_resolved: string;
+	export const SHLVL: string;
+	export const HOME: string;
+	export const VSCODE_GIT_ASKPASS_MAIN: string;
+	export const npm_package_dev_optional: string;
+	export const npm_config_cache: string;
+	export const LOGNAME: string;
+	export const npm_lifecycle_script: string;
+	export const VSCODE_GIT_IPC_HANDLE: string;
+	export const npm_config_user_agent: string;
+	export const GIT_ASKPASS: string;
+	export const VSCODE_GIT_ASKPASS_NODE: string;
+	export const npm_node_execpath: string;
+	export const npm_config_prefix: string;
+	export const COLORTERM: string;
+}
---
+++ .svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,168 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "vscode";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "/Users/aleja/Documents/DEV/ispc";
+
+/** @type {import('$env/static/private').TERM} */
+export const TERM = "xterm-256color";
+
+/** @type {import('$env/static/private').SHELL} */
+export const SHELL = "/bin/zsh";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').TMPDIR} */
+export const TMPDIR = "/var/folders/0m/lf4_96690s3g3q_4l9sf6y400000gn/T/";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "/usr/local";
+
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "1.67.2";
+
+/** @type {import('$env/static/private').ORIGINAL_XDG_CURRENT_DESKTOP} */
+export const ORIGINAL_XDG_CURRENT_DESKTOP = "undefined";
+
+/** @type {import('$env/static/private').npm_package_optional} */
+export const npm_package_optional = "";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "/Users/aleja/Documents/DEV/ispc";
+
+/** @type {import('$env/static/private').USER} */
+export const USER = "aleja";
+
+/** @type {import('$env/static/private').COMMAND_MODE} */
+export const COMMAND_MODE = "unix2003";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "/usr/local/etc/npmrc";
+
+/** @type {import('$env/static/private').npm_package_peer} */
+export const npm_package_peer = "";
+
+/** @type {import('$env/static/private').SSH_AUTH_SOCK} */
+export const SSH_AUTH_SOCK = "/private/tmp/com.apple.launchd.nv6IC31okM/Listeners";
+
+/** @type {import('$env/static/private').__CF_USER_TEXT_ENCODING} */
+export const __CF_USER_TEXT_ENCODING = "0x1F5:0:86";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "/usr/local/lib/node_modules/npm/bin/npm-cli.js";
+
+/** @type {import('$env/static/private').npm_package_integrity} */
+export const npm_package_integrity = "sha512-jHSa74F7k+hC+0fof75g/xm/+1M5sM66Qt6v8eLLMSgjkp36Lb5xOioBhbl6w0NYoE5xysLsBWuu+yHytfvCBA==";
+
+/** @type {import('$env/static/private').PATH} */
+export const PATH = "/Users/aleja/Documents/DEV/ispc/node_modules/@sveltejs/kit/node_modules/.bin:/Users/aleja/Documents/DEV/ispc/node_modules/@sveltejs/node_modules/.bin:/Users/aleja/Documents/DEV/ispc/node_modules/node_modules/.bin:/Users/aleja/Documents/DEV/ispc/node_modules/.bin:/Users/aleja/Documents/DEV/node_modules/.bin:/Users/aleja/Documents/node_modules/.bin:/Users/aleja/node_modules/.bin:/Users/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "/Users/aleja/Documents/DEV/ispc/node_modules/@sveltejs/kit/package.json";
+
+/** @type {import('$env/static/private')._} */
+export const _ = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "/Users/aleja/.npmrc";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "/Users/aleja/.npm-init.js";
+
+/** @type {import('$env/static/private').__CFBundleIdentifier} */
+export const __CFBundleIdentifier = "com.microsoft.VSCode";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "install";
+
+/** @type {import('$env/static/private').PWD} */
+export const PWD = "/Users/aleja/Documents/DEV/ispc/node_modules/@sveltejs/kit";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "postinstall";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "vi";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "@sveltejs/kit";
+
+/** @type {import('$env/static/private').LANG} */
+export const LANG = "es_419.UTF-8";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_EXTRA_ARGS} */
+export const VSCODE_GIT_ASKPASS_EXTRA_ARGS = "--ms-enable-electron-run-as-node";
+
+/** @type {import('$env/static/private').XPC_FLAGS} */
+export const XPC_FLAGS = "0x0";
+
+/** @type {import('$env/static/private').npm_package_engines_node} */
+export const npm_package_engines_node = ">=16.9";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "/usr/local/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+
+/** @type {import('$env/static/private').npm_package_dev} */
+export const npm_package_dev = "1";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "1.0.0-next.405";
+
+/** @type {import('$env/static/private').XPC_SERVICE_NAME} */
+export const XPC_SERVICE_NAME = "0";
+
+/** @type {import('$env/static/private').npm_package_resolved} */
+export const npm_package_resolved = "https://registry.npmjs.org/@sveltejs/kit/-/kit-1.0.0-next.405.tgz";
+
+/** @type {import('$env/static/private').SHLVL} */
+export const SHLVL = "2";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "/Users/aleja";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_MAIN} */
+export const VSCODE_GIT_ASKPASS_MAIN = "/private/var/folders/0m/lf4_96690s3g3q_4l9sf6y400000gn/T/AppTranslocation/634825FB-37FE-47DE-BC58-9D89801B9A71/d/Visual Studio Code.app/Contents/Resources/app/extensions/git/dist/askpass-main.js";
+
+/** @type {import('$env/static/private').npm_package_dev_optional} */
+export const npm_package_dev_optional = "";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "/Users/aleja/.npm";
+
+/** @type {import('$env/static/private').LOGNAME} */
+export const LOGNAME = "aleja";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "node svelte-kit.js sync";
+
+/** @type {import('$env/static/private').VSCODE_GIT_IPC_HANDLE} */
+export const VSCODE_GIT_IPC_HANDLE = "/var/folders/0m/lf4_96690s3g3q_4l9sf6y400000gn/T/vscode-git-7e75700966.sock";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/8.15.0 node/v18.7.0 darwin x64 workspaces/false";
+
+/** @type {import('$env/static/private').GIT_ASKPASS} */
+export const GIT_ASKPASS = "/private/var/folders/0m/lf4_96690s3g3q_4l9sf6y400000gn/T/AppTranslocation/634825FB-37FE-47DE-BC58-9D89801B9A71/d/Visual Studio Code.app/Contents/Resources/app/extensions/git/dist/askpass.sh";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_NODE} */
+export const VSCODE_GIT_ASKPASS_NODE = "/private/var/folders/0m/lf4_96690s3g3q_4l9sf6y400000gn/T/AppTranslocation/634825FB-37FE-47DE-BC58-9D89801B9A71/d/Visual Studio Code.app/Contents/MacOS/Electron";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "/usr/local";
+
+/** @type {import('$env/static/private').COLORTERM} */
+export const COLORTERM = "truecolor";
