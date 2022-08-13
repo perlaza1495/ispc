223a9cf680042f39ea142502bc748e68fe570380
Juan David <juamcaza@gmail.com>
2022-08-13 17:37:50 -0500

    Segundo comit Juan

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -3,13 +3,15 @@ export { matchers } from './client-matchers.js';
 export const components = [
 	() => import("..\\..\\src\\routes\\__layout.svelte"),
 	() => import("..\\runtime\\components\\error.svelte"),
+	() => import("..\\..\\src\\routes\\agendamiento.svelte"),
 	() => import("..\\..\\src\\routes\\agendar.svelte"),
 	() => import("..\\..\\src\\routes\\index.svelte"),
 	() => import("..\\..\\src\\routes\\opciones.svelte")
 ];
 
 export const dictionary = {
-	"": [[0, 3], [1]],
-	"agendar": [[0, 2], [1]],
-	"opciones": [[0, 4], [1]]
+	"": [[0, 4], [1]],
+	"agendamiento": [[0, 2], [1]],
+	"agendar": [[0, 3], [1]],
+	"opciones": [[0, 5], [1]]
 };
---
+++ src/routes/agendamiento.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<main>
+    <h1>Agendamiento</h1>
+</main>
+
+<style>
+
+</style>
